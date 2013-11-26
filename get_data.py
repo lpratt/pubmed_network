a0679efe2ca38fe56098efdf3c5b652414e4c352
Erik Islo <eislo@wesleyan.edu>
2013-11-26 22:51:37 +0100

    it works but a lot more is going to change.

--- get_data.py
+++ get_data.py
@@ -22,7 +22,7 @@ def gatherData(search_term, email="lwrpratt@gmail.com"):
   search_term = sys.argv[1]
 
   # Search for the term, create list of matching paper's ID's
-  handle = Entrez.esearch(db="pubmed", term=searchterm)
+  handle = Entrez.esearch(db="pubmed", term=search_term)
   record_for_id = Entrez.read(handle)
   idlist = record_for_id["IdList"]
   handle_medline = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
@@ -30,14 +30,14 @@ def gatherData(search_term, email="lwrpratt@gmail.com"):
   records = list(records)
 
   data = {}
-  with open("{}.json".format(searchterm.replace(' ', '_')), "w") as f:
+  with open("{}.json".format(search_term.replace(' ', '_')), "w") as f:
     for record in records:
-      data[record.get("PMID", "?")] = {"title" : record.get("MH", "?")}, "author(s)" : record.get("FAU", "?"), "keywords" : record.get("MH", "?")}
+      data[record.get("PMID", "?")] = {"title": record.get("TI", "?"), "author(s)": record.get("FAU", "?"), "keywords": record.get("MH", "?")}
       json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '))
     f.closed
 
 
-def readData(fname="data.json"):
+def readData(fname):
   json_data = open(fname, "r")
   read_data = json.load(json_data)
   json_data.close()
@@ -47,9 +47,7 @@ def readData(fname="data.json"):
 
 
 def appendToJSON(fname="data.json"):
-  searchterm =  sys.argv[1]
-
-  readData = readData(fname)
+  read_data = readData(fname)
 
   json_termfile = open(searchterm, "r")
   read_term_data = json.load(json_termfile)
@@ -59,17 +57,16 @@ def appendToJSON(fname="data.json"):
   # appends new data together
   read_data.update(read_term_data)
 
-  with open(fname, "w") as f:
+  with open(fname, "wb") as f:
     json.dump(read_data, f, sort_keys=True, indent=4, separators=(', ', ': '))
   f.close
 
 # Script starts here
 print "arguments: 1-search term, 2-filename," # 3-email"
 search_term = sys.argv[1]
-fname = sys.argv[2]
+# fname = sys.argv[2]
 # email = sys.argv[3] not implemented yet
 
 print "gathering data"
-getData(search_term)
+gatherData(search_term)
 print "writing to file"
-appendToJSON(fname)
