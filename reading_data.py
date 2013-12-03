a57fca997a97b2533097d8f50ed29e8187dd5129
Erik Islo <eislo@wesleyan.edu>
2013-12-03 15:16:52 +0100

    cleaned up some unneeded files

--- reading_data.py
+++
@@ -1,9 +0,0 @@
-# Reads data.json into a python dictionary
-
-import json
-
-# Reads in json file
-json_data = open("data.json", "r")
-read_data = json.load(json_data)
-json_data.close()
-print read_data
